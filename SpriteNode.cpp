a924d51b45ae54409bfec28613f31895e8266276
Mahmoud <hodaman2012@gmail.com>
2019-09-01 05:15:56 +0200

    world completed

--- SpriteNode.cpp
+++ SpriteNode.cpp
@@ -3,14 +3,20 @@
 //i stoped here at 7::20 am
 //i must fiz this shit to make background works
 
+
+
+SpriteNode::SpriteNode(const sf::Texture & texture):mSprite(texture)
+{
+//std::cout << "settttted" << std::endl;
+
+}
+
+SpriteNode::SpriteNode(const sf::Texture & texture, const sf::IntRect & rect):mSprite(texture,rect)
+{//	std::cout << "what the helllllll" << std::endl;
+}
+
 void SpriteNode::drawCurrent(sf::RenderTarget & target, sf::RenderStates states) const
 {
-}
-
-SpriteNode::SpriteNode(const sf::Texture & texture)
-{
-}
-
-SpriteNode::SpriteNode(const sf::Texture & texture, const sf::IntRect & rect)
-{
+	target.draw(mSprite, states);
+//	std::cout << "size.x"<<target.getSize().x<< std::endl;
 }
